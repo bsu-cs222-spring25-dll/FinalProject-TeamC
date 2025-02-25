84528303169b3d0573edc2aecf7984e012eca5ca
Dominick Smith <dominickssmith0903@gmail.com>
2025-02-25 12:02:23 -0500

    Added a test for the JSONparser

---
+++ src/test/java/JSONparserTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class JSONparserTest {
+    @Test
+    public void parserTest() throws IOException {
+        JSONparser parser = new JSONparser();
+        String base = "EUR";
+        String target = "USD";
+        InputStream JSONdata = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.json");
+        Double result = parser.parse(base,target,JSONdata);
+        Assertions.assertEquals(1.047916,result);
+    }
+}
