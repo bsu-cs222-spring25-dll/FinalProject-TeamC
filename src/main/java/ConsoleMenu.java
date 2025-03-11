5bb95aba2594ecb09d9f7c3334ef938178438a0c
Isaac Vance <ivance5373@gmail.com>
2025-03-11 11:56:56 -0400

    Rewrote existing menu code and added quit functionality

--- src/main/java/ConsoleMenu.java
+++ src/main/java/ConsoleMenu.java
@@ -1,22 +1,37 @@
 import java.io.IOException;
 import java.util.Scanner;
 public class ConsoleMenu {
+    Scanner scan = new Scanner(System.in);
+    boolean quit = false;
     public void runMenu() throws IOException {
         JSONDataGetter dataGetter = new JSONDataGetter();
         APIConnection apiConnection = new APIConnection();
         String data = dataGetter.dataGetter(apiConnection.encodedUrlString());
-        Scanner scan = new Scanner(System.in);
-        System.out.println("""
-                ***Welcome***
-                Select an option
-                1. See all rates
-                2. Convert money amount
-                3. Quit""");
-        int userInput = scan.nextInt();
-        switch (userInput){
-            case 1:
-                System.out.println(data);
+        boolean quit = false;
+        while(!quit) {
+            System.out.println("""
+                    ***Welcome***
+                    Select an option
+                    1. See all rates
+                    2. Convert money amount
+                    3. Quit""");
+            choiceCase(data);
         }
+    }
+
+public void choiceCase(String data){
+    int userInput = scan.nextInt();
+    switch (userInput) {
+        case 1:
+            System.out.println(data);
+            break;
+        case 2:
+            System.out.println("");
+            break;
+        case 3:
+            quit = true;
+        }
+
 
     }
 }
