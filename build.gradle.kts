6ec1ac6b77a7308768b08d91d127af180b4b07d4
Noah Silva-Lopez <117937042+NFSilvaLopez@users.noreply.github.com>
2025-03-25 11:04:18 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    java
+    application
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "org.example"
@@ -15,7 +17,16 @@ dependencies {
     implementation("com.jayway.jsonpath:json-path:2.9.0")
     implementation("net.minidev:json-smart:2.5.0")
     implementation("org.slf4j:slf4j-nop:2.0.11")
+    testImplementation("ch.qos.logback:logback-classic:1.4.12")
+}
 
+javafx {
+    version = "23.0.2"
+    modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+application {
+    mainClass.set("edu.bsu.cs.GUI") // Fully qualified name
 }
 
 tasks.test {
