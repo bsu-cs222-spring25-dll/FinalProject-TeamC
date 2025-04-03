f056677f975550f70c486f1fdbeae9e4f2fb085f
Dominick Smith <dominickssmith0903@gmail.com>
2025-04-03 12:00:07 -0400

    Started Error Handling for API Connection

--- src/main/java/edu/bsu/cs/APIConnection.java
+++ src/main/java/edu/bsu/cs/APIConnection.java
@@ -10,12 +10,21 @@ public class APIConnection extends JSONDataGetter{
     @Deprecated
     static String APIKey = "92a4a7a22301e1794c33f6f39f3db1a8";
     static URL APIUrl;
+
     public static URLConnection encodedUrlString() throws IOException {
+        ErrorReport error = new ErrorReport();
+
         String encodedUrlString = "http://api.exchangeratesapi.io/v1/latest?access_key=" + URLEncoder.encode(APIKey) + "&format=1";
         APIUrl = new URL(encodedUrlString);
-        URLConnection connection = APIUrl.openConnection();
-        connection.connect();
-        return connection;
+        String checkConnection = error.chechkConnectionStatus(APIUrl);
+        if (checkConnection.equals("There was a network error; could not connect to the internet")) {
+            System.out.println(checkConnection);
+            return null;
+        }else{
+            URLConnection connection = APIUrl.openConnection();
+            connection.connect();
+            return connection;
+        }
     }
 
     public static String getUrlAsStr(String base){
