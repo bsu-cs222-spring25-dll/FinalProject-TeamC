edcc91d68d32b4c257635a37c183498d3703dd1a
Christian Johnson <5419cjohnson@gmail.com>
2025-04-07 09:10:54 -0400

    Adds ErrorPrinter and errors to APIConnection

--- src/main/java/edu/bsu/cs/APIConnection.java
+++ src/main/java/edu/bsu/cs/APIConnection.java
@@ -1,34 +1,75 @@
 package edu.bsu.cs;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.Properties;
 
 @SuppressWarnings("ALL")
 public class APIConnection extends JSONDataGetter{
     @Deprecated
-    static String APIKey = "92a4a7a22301e1794c33f6f39f3db1a8";
+    static Properties properties = new Properties();
+    static ErrorReport errorReport = new ErrorReport();
+    static ErrorPrinter errorPrinter = new ErrorPrinter();
+
+
+    static String APIKey;
     static URL APIUrl;
+    static String connectionStatusMessage;
+    static int responseCode;
+    static HttpsURLConnection APIconnection;
+    static String APIUsageMessage;
 
-    public static URLConnection encodedUrlString() throws IOException {
-        ErrorReport error = new ErrorReport();
+    public static HttpsURLConnection encodedUrlString() throws IOException {
+        APIKey = getAPIKey();
+        try {
+            APIUrl = new URL("https://api.exchangeratesapi.io/v1/latest?access_key=" + APIKey + "&format=1");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
 
-        String encodedUrlString = "http://api.exchangeratesapi.io/v1/latest?access_key=" + URLEncoder.encode(APIKey) + "&format=1";
-        APIUrl = new URL(encodedUrlString);
-        String checkConnection = error.chechkConnectionStatus(APIUrl);
-        if (checkConnection.equals("There was a network error; could not connect to the internet")) {
-            System.out.println(checkConnection);
-            return null;
-        }else{
-            URLConnection connection = APIUrl.openConnection();
-            connection.connect();
-            return connection;
+        try {
+            checkConnectionError(APIUrl);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            APIconnection = (HttpsURLConnection) APIUrl.openConnection();
+            check429Error(APIconnection);
+            return APIconnection;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+//    public static String getUrlAsStr(String base){
+//        String encodedUrlString = "http://api.exchangeratesapi.io/v1/latest?access_key=" + URLEncoder.encode(APIKey) + URLEncoder.encode(base) + "&format=1";
+//        return encodedUrlString;
+//    }
+
+    public static String getAPIKey() {
+        try (InputStream inputStream = APIConnection.class.getClassLoader().getResourceAsStream("config.properties")) {
+            properties.load(inputStream);
+            return properties.getProperty("apiKey");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
-    public static String getUrlAsStr(String base){
-        String encodedUrlString = "http://api.exchangeratesapi.io/v1/latest?access_key=" + URLEncoder.encode(APIKey) + URLEncoder.encode(base) + "&format=1";
-        return encodedUrlString;
+    public static void checkConnectionError(URL url) throws IOException {
+        connectionStatusMessage = errorReport.chechkConnectionStatus(url);
+        errorPrinter.printConnectionMessageError(connectionStatusMessage);
+    }
+
+    public static void check429Error(HttpsURLConnection connection) throws IOException {
+        responseCode = connection.getResponseCode();
+        APIUsageMessage = errorReport.check429Status(responseCode);
+        errorPrinter.print429Error(APIUsageMessage);
     }
 }
---
+++ src/main/java/edu/bsu/cs/ErrorPrinter.java
@@ -0,0 +1,17 @@
+package edu.bsu.cs;
+
+public class ErrorPrinter {
+    public void printConnectionMessageError(String connectionMessage) {
+        if (!connectionMessage.isEmpty()) {
+            System.err.println(connectionMessage);
+        }
+
+
+    }
+
+    public void print429Error(String error429Message) {
+        if (!error429Message.isEmpty()) {
+            System.err.println(error429Message);
+        }
+    }
+}
