ca7a515e77275a144722161f8e279c0328eabd5f
noahsilva-lopez <noahsilvalopez@icloud.com>
2025-04-13 15:38:13 -0400

    updated version tag in menu.

--- src/main/java/edu/bsu/cs/GUI.java
+++ src/main/java/edu/bsu/cs/GUI.java
@@ -74,7 +74,7 @@ public class GUI extends Application {
         });
 
 
-        Label versionLabel = new Label("Version 0.2.4");
+        Label versionLabel = new Label("Version 0.3.1");
         versionLabel.setFont(Font.font("Arial", 12));
         versionLabel.setTextFill(Color.web("#7f8c8d"));
 
